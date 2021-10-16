8a71f8e39244ce90a7626f926cc668e9351c48cd
zsarge <zackarywsargent@gmail.com>
2021-10-16 16:16:57 -0400

    add basic image generation

--- main.js
+++ main.js
@@ -4,21 +4,29 @@ import data from "./urls.json" assert { type: "json" };
 let urls = data.urls;
 
 function* generateUrls() {
-    while (true) {
-        if (urls.length == 0) {
-            urls = data.urls;
-            console.debug("reset")
-        }
-        // select a random url
-        let randChoice = urls[Math.floor(Math.random() * urls.length)];
-        // remove it from the list
-        urls = urls.filter(url => url !== randChoice);
-        // if we have used all the urls, reset the list
-        yield randChoice;
+  while (true) {
+    if (urls.length == 0) {
+      urls = data.urls;
+      console.debug("reset");
     }
+    // select a random url
+    let randChoice = urls[Math.floor(Math.random() * urls.length)];
+    // remove it from the list
+    urls = urls.filter((url) => url !== randChoice);
+    // if we have used all the urls, reset the list
+    yield randChoice;
+  }
 }
-
 
 for (let i = 0; i < 30; i++) {
-    console.log(generateUrls().next().value)
+  console.log(generateUrls().next().value);
 }
+
+function setNewRandomImage() {
+  const img = document.getElementById("game-display");
+  img.src = generateUrls().next().value;
+}
+
+setNewRandomImage();
+
+document.getElementById("next").addEventListener("click", setNewRandomImage);
