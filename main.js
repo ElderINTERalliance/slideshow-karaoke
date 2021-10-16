c18ba3d1e2872c2bab4e6d5fc26e6503baa5c573
zsarge <zackarywsargent@gmail.com>
2021-10-16 15:39:00 -0400

    choose random urls

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("javascript loaded correctly")
+"use strict";
+
+import data from "./urls.json" assert { type: "json" };
+const urls = data.urls;
+
+function* generateUrls() {
+    while (true) {
+        // select a random url
+        yield urls[Math.floor(Math.random() * urls.length)];
+    }
+}
+
+console.log(generateUrls().next().value)
+console.log(generateUrls().next().value)
+console.log(generateUrls().next().value)
