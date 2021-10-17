856ad8b905a186089728bac2ca3f9d7015264758
zsarge <zackarywsargent@gmail.com>
2021-10-16 22:55:56 -0400

    add slide limit

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 "use strict";
 
+// Note that a const binding != const values
+const settings = {
+  ALLOWED_TYPES: ["slides"],
+  limit: {
+    type: "slides",
+    amount: 10,
+  },
+};
+
+function showInputError(message) {
+  document.getElementById("input-error-message").textContent = message;
+}
+
+// state management
+function startGame() {
+  const type = document.getElementById("limit-type").value;
+  const amount = document.getElementById("amount").value;
+
+  if (parseInt(amount) == NaN || parseInt(amount) < 1) {
+    showInputError("Please enter a number amount greater than 1");
+    return;
+  }
+  if (!settings.ALLOWED_TYPES.includes(type)) {
+    showInputError("Please choose an allowed type");
+    return;
+  }
+
+  settings.limit.amount = parseInt(amount);
+  settings.limit.type = type;
+
+  // show the user the game
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("end-screen").style.display = "none";
+  document.getElementById("game-window").style.display = "flex";
+}
+
+function endGame() {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("game-window").style.display = "none";
+  document.getElementById("end-screen").style.display = "block";
+}
+
+document.getElementById("start-button").addEventListener("click", startGame);
+
+// game logic
 (async () => {
   const response = await fetch("./urls.json");
   if (response.status !== 200) {
@@ -31,7 +76,8 @@
   }
 
   class SlideShow {
-    constructor() {
+    constructor(SETTINGS) {
+      this.SETTINGS = SETTINGS;
       // slides is a list of URLs
       this.slides = [];
       this.position = -1; // this will be immediately incremented
@@ -44,6 +90,16 @@
         img.src = url;
       })();
     }
+    _shouldEndGame() {
+      switch (this.SETTINGS.limit.type) {
+        case "slides":
+          // remember this.position is zero-indexed
+          return this.position >= this.SETTINGS.limit.amount;
+
+        default:
+          return true;
+      }
+    }
     _update() {
       // make sure we always have 5 preloaded slides ready
       while (this.position > this.slides.length - this.PRELOAD_LIMIT) {
@@ -51,6 +107,13 @@
         this._preloadImage(url);
         this.slides.push(url);
       }
+      if (this._shouldEndGame()) {
+        endGame();
+      }
+    }
+    _setImageToCurrentPosition(url) {
+      const display = document.getElementById("game-display");
+      display.src = this.slides[this.position];
     }
     next() {
       this.position++;
@@ -62,13 +125,9 @@
       this._update();
       this._setImageToCurrentPosition();
     }
-    _setImageToCurrentPosition(url) {
-      const display = document.getElementById("game-display");
-      display.src = this.slides[this.position];
-    }
   }
 
-  const presentation = new SlideShow();
+  const presentation = new SlideShow(settings);
 
   // note the anonymous function is necessary to maintain the global scope
   document
