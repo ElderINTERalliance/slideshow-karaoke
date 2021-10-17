732e3cbae5745a517c6396b40c314d96ff4c1367
zsarge <zackarywsargent@gmail.com>
2021-10-16 20:02:08 -0400

    defintions don't need to be async

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
 "use strict";
 
+function* generateUrls() {
+  while (true) {
+    if (urls.length == 0) {
+      urls = data.urls;
+      console.debug("reset");
+    }
+    // select a random url
+    let randChoice = urls[Math.floor(Math.random() * urls.length)];
+    // remove it from the list
+    urls = urls.filter((url) => url !== randChoice);
+    // if we have used all the urls, reset the list
+    console.debug(randChoice);
+    yield randChoice;
+  }
+}
+
+class SlideShow {
+  constructor() {
+    // slides is a list of URLs
+    this.slides = [];
+    this.position = -1; // this will be immediately incremented
+    this.PRELOAD_LIMIT = 5;
+  }
+  // asynchronous image preload
+  _preloadImage(url) {
+    (async () => {
+      var img = new Image();
+      img.src = url;
+    })();
+  }
+  _update() {
+    // make sure we always have 5 preloaded slides ready
+    while (this.position > this.slides.length - this.PRELOAD_LIMIT) {
+      let url = generateUrls().next().value;
+      this._preloadImage(url);
+      this.slides.push(url);
+    }
+  }
+  next() {
+    this.position++;
+    this._update();
+    this._setImageToCurrentPosition();
+  }
+  last() {
+    this.position--;
+    this._update();
+    this._setImageToCurrentPosition();
+  }
+  _setImageToCurrentPosition(url) {
+    const display = document.getElementById("game-display");
+    display.src = this.slides[this.position];
+  }
+}
+
 (async () => {
   const response = await fetch("./urls.json");
   if (response.status !== 200) {
@@ -11,62 +65,8 @@
   const data = await response.json();
 
   let urls = data.urls;
-  console.log(data);
-  console.log(urls);
-
-  function* generateUrls() {
-    while (true) {
-      if (urls.length == 0) {
-        urls = data.urls;
-        console.debug("reset");
-      }
-      // select a random url
-      let randChoice = urls[Math.floor(Math.random() * urls.length)];
-      // remove it from the list
-      urls = urls.filter((url) => url !== randChoice);
-      // if we have used all the urls, reset the list
-      console.debug(randChoice);
-      yield randChoice;
-    }
-  }
-
-  class SlideShow {
-    constructor() {
-      // slides is a list of URLs
-      this.slides = [];
-      this.position = -1; // this will be immediately incremented
-      this.PRELOAD_LIMIT = 5;
-    }
-    // asynchronous image preload
-    _preloadImage(url) {
-      (async () => {
-        var img = new Image();
-        img.src = url;
-      })();
-    }
-    _update() {
-      // make sure we always have 5 preloaded slides ready
-      while (this.position > this.slides.length - this.PRELOAD_LIMIT) {
-        let url = generateUrls().next().value;
-        this._preloadImage(url);
-        this.slides.push(url);
-      }
-    }
-    next() {
-      this.position++;
-      this._update();
-      this._setImageToCurrentPosition();
-    }
-    last() {
-      this.position--;
-      this._update();
-      this._setImageToCurrentPosition();
-    }
-    _setImageToCurrentPosition(url) {
-      const display = document.getElementById("game-display");
-      display.src = this.slides[this.position];
-    }
-  }
+  console.debug(data);
+  console.debug(urls);
 
   const presentation = new SlideShow();
 
