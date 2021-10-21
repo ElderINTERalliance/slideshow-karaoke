1aa7cd039d6dff53e32e8fa518c930d89966579e
zsarge <zackarywsargent@gmail.com>
2021-10-21 17:42:42 -0400

    update todo

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ function startTimer(endTime /* as a Date */) {
     // Find the distance between now and the count down date
     var distance = endTime.getTime() - now;
 
-    // Time calculations for days, hours, minutes and seconds
+    // TODO: Fix when distance >= 60min
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
